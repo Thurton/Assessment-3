8cdc4175e34efac22ca9d7cb2610635e768f6c8d
Thurton <bradjl85@gmail.com>
2015-06-15 13:15:54 +0930

    Main.js file added with delta time and canvas info.

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+
+
+function getDeltaTime()
+{
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+	
+		
+	if(deltaTime > 1)
+		deltaTime = 1;
+		
+	return deltaTime;
+}
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
