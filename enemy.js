e2383b603d80d8ca269f28d2676ee7871394e428
Thurton <bradjl85@gmail.com>
2015-06-15 12:38:33 +0930

    Added index so we can work on other .js files and make sure they work. Some enemy.js code is in need to update and modify

---
+++ enemy.js
@@ -0,0 +1,83 @@
+var Enemy = function(x, y) {
+	
+	this.sprite = new Sprite //();
+	
+	this.sprite.buildAnimation//();
+	this.sprite.setAnimationOffset//();
+
+	
+	
+	
+	this.width = //;
+	this.height = //;
+	
+	this.position = new Vector2();
+	this.position.set(x, y);
+	
+	this.velocity = new Vector2();
+	
+	this.moveRight = true;
+	this.pause = 0;
+}
+
+
+
+
+Enemy.prototype.update = function(dt)
+{
+	this.sprite.update(dt);
+	
+	if(this.pause > 0)
+	{
+		this.pause -= dt;
+	}
+	else
+	{
+		var ddx = 0;
+		
+		var tx = pixelToTile(this.position.x);
+		var ty = pixelToTile(this.position.y);
+		var nx = (this.position.x)%TILE;
+		var ny = (this.position.y)%TILE;
+		var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+		var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx, + 1, ty);
+		var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
+		var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx +1, ty + 1);
+		
+		if(this.moveRight)
+		{
+			if(celldiag && !cellright){
+				ddx = ddx + ENEMY_ACCEL;
+				  
+			}
+			else{
+				this.velocity.x = 0;
+				this.moveRight = false;
+				this.pause = 0.5;
+			}
+			  }
+            if(!this.moveRight)
+            {
+                if(celldown && !cell) {
+                    ddx = ddx - ENEMY_ACCEL; 
+                    
+                }
+                else {
+                    this.velocity.x = 0;
+                    this.moveRight = true;
+                    this.pause = 0.5;
+                    
+                }
+            
+			}
+		
+		this.position.x = Math.floor(this.position.x + (dt * this.velocity.x));
+		this.velocity.x = bound(this.velocity.x + (dt * ddx),
+		-ENEMY_MAXDX, ENEMY_MAXDX);
+		}
+	}
+	
+	Enemy.prototype.draw = function()
+{
+	this.sprite.draw(context, this.position.x - worldOffsetX, this.position.y);
+}
