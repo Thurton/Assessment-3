84f6df4afc043f495ee1c949cd1b67950c976eca
Rebtand <u.ryantoo@gmail.com>
2015-06-17 13:43:10 +1000

    added state manager and splash screen state

---
+++ splashstate.js
@@ -0,0 +1,28 @@
+
+var SplashState = function() 
+{
+    this.prototype = BaseState;
+}
+
+SplashState.prototype.load = function() 
+{
+}
+
+SplashState.prototype.unload = function() 
+{
+}
+
+SplashState.prototype.update = function(dt) 
+{
+    if( keyboard.isKeyDown( keyboard.KEY_ENTER ) == true)
+    {
+        stateManager.switchState(new GameState());
+    }
+}
+
+SplashState.prototype.draw = function() 
+{
+    var splashimg = document.createElement("img");
+    splashimg.src = "assets/Splash_Screen.png"
+    context.drawImage(splashimg, 0, 0)
+}
