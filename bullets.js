be80a2a3eda7b94aba07b0712ac993080b808c81
Thorrack78 <Thorrack78@gmail.com>
2015-06-18 10:19:43 +1000

    Minor Changes

--- bullets.js
+++ bullets.js
@@ -1,25 +1,57 @@
-var Bullet = function(x, y)
+var BULLET_UP = 0
+var BULLET_DOWN = 1
+var Bullet = function(x, y, moveRight, moveUp)
 {
-	this.sprite = new Sprite("assets\bullet.png");
-	this.sprite = buildAnimation(1, 1, 32, 32, -1, [0]);
-	this.sprite.setAnimationOffset (0, 0, 0)
-	this.sprite.setLoop(0, false)
+    this.sprite = new Sprite("assets/bullet.png");
+    this.sprite.buildAnimation(2, 1, 34, 17, 0.2, [1]);
+    this.position = new Vector2(x, y);
+    this.velocity = new Vector2();
+    
+    this.moveRight = moveRight;
+    if(this.moveRight == true)
+        this.velocity.set(MAXDX *0, 2);
+    else
+        this.velocity.set(-MAXDX *0, 2);
+    if (this.velocity.x > 0)
+    {
+        this.sprite.setAnimation(BULLET_UP);
+    }
+    if (this.velocity.x < 0)
+    {
+        this.sprite.setAnimation(BULLET_UP);
+    }
 	
-	this.position = new Vector2();
-	this.position.set (x, y);
-	
-	this.velocity = new Vector2();
-	
-	
-	Bullet.prototype.update = function(dt)
-	{
-		this.sprite.update(dt);
-		this.position.y = Math.floor(this.position.y) + (dt * this.velocity.y);
-	}
-	
-	Bullet.prototype.draw = function()
-	{
-		var.screenY = this.position.y - worldOffsetY;
-		this.sprite.draw(context, screenY, this.position.x)
-	}
+	this.moveUp = moveUp;
+    if(this.moveUp == true)
+        this.velocity.set(MAXDX *0, 2);
+    else
+        this.velocity.set(-MAXDX *0, 2);
+    if (this.velocity.x > 0)
+    {
+        this.sprite.setAnimation(BULLET_UP);
+    }
+    if (this.velocity.x < 0)
+    {
+        this.sprite.setAnimation(BULLET_UP);
+    }
 }
+
+Bullet.prototype.update = function(dt)
+{
+    this.sprite.update(dt);
+    this.position.y = Math.floor(this.position.y + (dt * this.velocity.y));
+}
+
+Bullet.prototype.draw = function()
+{
+    var screenY = this.position.y - worldOffsetY;
+    this.sprite.draw(context, screenY, this.position.x);
+
+
+
+// For Main.js
+
+var bullets = [];
+var MAXDX = METER * 10;
+var MAXDY = METER * 15;
+var ACCEL = MAXDX * 2;
