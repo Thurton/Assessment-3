9a03a3dfbea49054dd3dbfd9c8e97ac3d276c1e1
AiiDynBrus <aiidyn1998@hotmail.com>
2015-06-22 12:54:14 +1000

    Changed the dimensions of lives and score.             -Aidyn

--- main.js
+++ main.js
@@ -49,14 +49,14 @@ function run()
 	 context.fillStyle = "yellow";
 	 context.font= "25px Arial";
 	 var scoreText = "SCORE: " + score;
-	 context.fillText(scoreText, SCREEN_WIDTH - 265, 31);
+	 context.fillText(scoreText, SCREEN_WIDTH - 827, 660);
 	 
 	//LIFE COUNTER
 	 for(var i=0; i<lives; i++)
 	 {
 	 // 10 is the distance from the top of context
 	 // 20 is the gap between the lives icons
-		 context.drawImage (heartImage, 30*i, 10);
+		 context.drawImage (heartImage, 100*i, 660);
 	 }
 
 }
