970b321fb1984dd5c5cc1a125099e1e9a723068d
AiiDynBrus <aiidyn1998@hotmail.com>
2015-06-22 13:41:13 +1000

    Edited main.js run function. moved most code to gamerun.js        -Aidyn

--- main.js
+++ main.js
@@ -22,41 +22,11 @@ function getDeltaTime()
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
 
+
+
 function run()
 {
 	 context.fillStyle = "#ccc";
 	 context.fillRect(0, 0, canvas.width, canvas.height);
 	 
-	 drawMap();
-	 player.draw();
-	 
-	 //update the frame counter
-	 fpsTime += deltaTime;
-	 fpsCount++;
-	 if(fpstime >= 1)
-	 {
-		 fpsTime -= 1;
-		 fps = fpsCount;
-		 fpsCount = 0;
-	 }
-	 
-	//FPS
-	 context.fillStyle = "#f00";
-	 context.font="12px Arial";
-	 context.fillText("FPS: " + fps, 5, 20, 100);
-
-	//SCORE
-	 context.fillStyle = "yellow";
-	 context.font= "20px Arial";
-	 var scoreText = "SCORE: " + score;
-	 context.fillText(scoreText, SCREEN_WIDTH - 827, 660);
-	 
-	//LIFE COUNTER
-	 for(var i=0; i<lives; i++)
-	 {
-	 // 10 is the distance from the top of context
-	 // 20 is the gap between the lives icons
-		 context.drawImage (heartImage, 100*i, 660);
-	 }
-
 }
